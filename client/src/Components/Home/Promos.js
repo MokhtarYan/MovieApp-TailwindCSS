ba9e079cc70ef061f17f16a37fb0b8b59b67b07b
MokhtarYan <moalla.m.mokhtar@gmail.com>
2023-07-05 14:46:55 +0200

    add Top Rated

--- client/src/Components/Home/Promos.js
+++ client/src/Components/Home/Promos.js
@@ -1,7 +1,39 @@
 import React from "react";
-
+import { FiUser } from "react-icons/fi";
 const Promos = () => {
-  return <div>Promos</div>;
+  return (
+    <div className="my-20 py-10 md:px-20 px-8 bg-dry">
+      <div className="  lg:grid lg:grid-cols-2 lg:gap-10 items-center">
+        <div className="flex lg:gap-10 gap-6 flex-col">
+          <h1 className="xl:text-3xl text-xl capitalize font-sans font-medium xl:leading-relaxed ">
+            Watch your favourite movies
+          </h1>
+          Download Your Movies & Watch Offline. <br /> Enjoy on Your Mobile
+          <p className="text-text tex-sm xl:text-base leading-6 xl:leading-8">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt
+            perspiciatis aliquam impedit sapiente dolor eligendi iusto porro
+            ratione, alias accusamus, perferendis quas praesentium illum quia
+            corrupti animi. Nisi, exercitationem maiores.
+          </p>
+          <div className="flex gap-4 md:text-lg tex-sm">
+            <div className="flex-colo bg-black text-subMain px-6 py-3 rounded font-bold">
+              watch 4K HD
+            </div>
+            <div className="flex-rows gap-4  bg-black text-subMain px-6 py-3 rounded font-bold">
+              <FiUser />
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center">
+          <img
+            src="https://abdalmoniem.github.io/Movie-App/images/screenshot1.png"
+            className="h-auto w-40"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Promos;
--- client/src/Components/Home/TopRate.js
+++ client/src/Components/Home/TopRate.js
@@ -1,7 +1,41 @@
 import React from "react";
-
+import { Movies } from "../../Data/MoviesData";
+import { useState } from "react";
+import Titles from "../Titles";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { BsBookmarkStarFill } from "react-icons/bs";
+import { Autoplay, Navigation } from "swiper";
 const TopRate = () => {
-  return <div>TopRate</div>;
+  const [nextEl, setNextEl] = useState(null);
+  const [prevEl, setPrevEl] = useState(null);
+  return (
+    <div className="my-16">
+      <Titles title="Top Rated" Icon={BsBookmarkStarFill} />
+      <div className="mt-10">
+        <Swiper
+          navigation={{ nextEl, prevEl }}
+          slidesPerView={4}
+          spaceBetween={40}
+          autoplay={true}
+          speed={1000}
+          loop={true}
+          modules={[Navigation, Autoplay]}
+        >
+          {Movies.map((movie, index) => (
+            <SwiperSlide key={index}>
+              <div className="p-4 h-rate border border-border bg-dry rounded-lg overflow-hidden ">
+                <img
+                  src={`${movie.titleImage}`}
+                  alt={movie.name}
+                  className="w-full h-full object-cover rounded-lg"
+                />
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
+  );
 };
 
 export default TopRate;
